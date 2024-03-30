688f9a2268ae599e04b8f9549e323a2382ff08d9
Apisit B <virus_eddy@hotmail.com>
2024-03-30 14:46:44 +0700

    Update test.sql

--- testdemo/test.sql
+++ testdemo/test.sql
@@ -1,17 +1,19 @@
 DROP TABLE IF EXISTS rates;
 DROP TABLE IF EXISTS char_info;
 
-CREATE TABLE rates (
-    char_id VARCHAR PRIMARY KEY ,
-    rarity_star INT NOT NULL ,
-    color VARCHAR NOT NULL ,
-    rates REAL NOT NULL
-);
-CREATE TABLE char_info(
-    char_id VARCHAR PRIMARY KEY ,
-    skill TEXT NOT NULL ,
-    element  VARCHAR NOT NULL ,
+CREATE TABLE char_info (
+    char_id VARCHAR PRIMARY KEY,
+    skill TEXT NOT NULL,
+    element VARCHAR NOT NULL,
     rarity_star INT NOT NULL
 );
+
+CREATE TABLE rates (
+    char_id VARCHAR PRIMARY KEY,
+    color VARCHAR NOT NULL,
+    rates REAL NOT NULL,
+    FOREIGN KEY (char_id) REFERENCES char_info(char_id)
+);
+
 
 
