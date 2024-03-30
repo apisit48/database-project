30a7cbfb7b687685ab3197cf2492a6b044b6ff19
Apisit B <virus_eddy@hotmail.com>
2024-03-30 21:23:38 +0700

    Update test.sql

--- testdemo/test.sql
+++ testdemo/test.sql
@@ -14,6 +14,24 @@ CREATE TABLE rates (
     rates REAL NOT NULL,
     FOREIGN KEY (char_id) REFERENCES char_info(char_id)
 );
+CREATE TABLE pay_wall (
+    id SERIAL PRIMARY KEY,
+    price FLOAT NOT NULL,
+    amount INT NOT NULL,
+    limited_purchase BOOLEAN NOT NULL,
+    country VARCHAR NOT NULL,
+    exchange_rate INT NOT NULL
+);
+
+CREATE TABLE Banner (
+    banner_id SERIAL PRIMARY KEY,
+    amount_needed INT NOT NULL,
+    pity INT NOT NULL,
+    banner_info VARCHAR NOT NULL,
+    rates FLOAT NOT NULL,
+    char_id VARCHAR NOT NULL,
+    FOREIGN KEY (char_id) REFERENCES char_info(char_id)
+);
 
 
 
