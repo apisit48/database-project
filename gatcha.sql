2f4247b6901e26067a10798173a67ab8e9cacc7c
mikggi <90789712+mikggi@users.noreply.github.com>
2024-03-30 13:42:54 +0700

    sample table added

---
+++ gatcha.sql
@@ -0,0 +1,4 @@
+INSERT INTO tlb(char_name,id,rarity) VALUES ('Micheal',2,'0.1');
+INSERT INTO tlb(char_name,id,rarity) VALUES ('Gabirel',1,'0.1');
+INSERT INTO tlb(char_name,id,rarity) VALUES ('Uriel',3,'0.1');
+INSERT INTO tlb(char_name,id,rarity) VALUES ('Rapheal',4,'0.1');
