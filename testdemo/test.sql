08fcae6c7331ae406ac705add7e7b5f461f9a377
Apisit B <virus_eddy@hotmail.com>
2024-03-30 14:42:39 +0700

    commit test.sql

--- testdemo/test.sql
+++ testdemo/test.sql
@@ -1,4 +1,17 @@
-INSERT INTO tlb(char_name,id,rarity) VALUES ('Micheal',2,'0.1');
-INSERT INTO tlb(char_name,id,rarity) VALUES ('Gabirel',1,'0.1');
-INSERT INTO tlb(char_name,id,rarity) VALUES ('Uriel',3,'0.1');
-INSERT INTO tlb(char_name,id,rarity) VALUES ('Rapheal',4,'0.1');
+DROP TABLE IF EXISTS rates;
+DROP TABLE IF EXISTS char_info;
+
+CREATE TABLE rates (
+    char_id VARCHAR PRIMARY KEY ,
+    rarity_star INT NOT NULL ,
+    color VARCHAR NOT NULL ,
+    rates REAL NOT NULL
+);
+CREATE TABLE char_info(
+    char_id VARCHAR PRIMARY KEY ,
+    skill TEXT NOT NULL ,
+    element  VARCHAR NOT NULL ,
+    rarity_star INT NOT NULL
+);
+
+
